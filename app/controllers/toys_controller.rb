2470ba17f592b685fcaeface4a011bdf126211ad
Judy Mwai <juddiemukami@gmail.com>
2022-10-03 21:00:53 +0300

    done

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,13 +7,18 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
+    # if toy
     render json: toy, status: :created
+    # else 
+    #   render json: {error: "not_found"}
+    # end
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy
   end
 
   def destroy
